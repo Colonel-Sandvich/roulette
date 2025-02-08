c4de9730b8e5a0f7f818aae015098c02b4695b90
Colonel-Sandvich <21989418+Colonel-Sandvich@users.noreply.github.com>
2025-02-08 02:15:05 +0000

    Switch to start script since, volume is unavailable during pre-start

--- start.sh
+++ start.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-# Make sure this file has executable permissions, run `chmod +x pre-deploy.sh`
+# Make sure this file has executable permissions, run `chmod +x start.sh`
 
 # Exit the script if any command fails
 set -e
-
-echo "What is the value of DB_DATABASE";
-echo "$DB_DATABASE";
-
-if [ ! -d "/db"]; then
-    echo "/db does not exist!???!?!???!?!?!??!"
-    exit 1
-fi
-
-ls . / /db
 
 if [ ! -f "$DB_DATABASE" ]; then
     echo "Database file does not exist. Creating at: $DB_DATABASE"
@@ -32,3 +22,11 @@ php artisan migrate
 
 # Cache the various components of the Laravel application
 php artisan optimize
+
+# Set up nginx conf and run nginx
+node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf
+
+(
+    php-fpm -y /assets/php-fpm.conf &
+    nginx -c /nginx.conf
+)
