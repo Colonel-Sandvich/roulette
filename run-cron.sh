9492a7c498f8bea146d7422aa18d1ae57aaea78d
Colonel-Sandvich <21989418+Colonel-Sandvich@users.noreply.github.com>
2025-02-09 08:14:06 +0000

    Switch from bc to awk to calc sleep time

--- run-cron.sh
+++ run-cron.sh
@@ -6,12 +6,12 @@ while [ true ]; do
     # Get current time as a floating-point number (seconds + nanoseconds)
     current_time=$(date +%S.%N)
 
-    # Compute how many seconds to sleep until the next full minute
-    sleep_time=$(echo "60 - $current_time" | bc)
+    # Calculate time to sleep until the next full minute
+    sleep_time=$(awk "BEGIN {print 60 - $current_time}")
 
-#    echo "Waiting $sleep_time seconds for the next full minute..."
-    sleep $sleep_time  # Wait until `XX:YY:00.000`
+    # echo "Waiting $sleep_time seconds for the next full minute..."
+    sleep $sleep_time # Wait until `XX:YY:00.000`
 
-#    echo "Running scheduler at: $(date +'%Y-%m-%d %H:%M:%S.%3N')"
+    # echo "Running scheduler at: $(date +'%F %T.%3N')"
     php artisan schedule:run --verbose --no-interaction &
 done
