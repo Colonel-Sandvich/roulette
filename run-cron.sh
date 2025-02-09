b6943b8dccc094cf72570026e770f1ed6db1dc69
Colonel-Sandvich <21989418+Colonel-Sandvich@users.noreply.github.com>
2025-02-09 07:50:34 +0000

    Improve cron to use millisecond precision

--- run-cron.sh
+++ run-cron.sh
@@ -3,12 +3,15 @@
 
 # Run the Laravel scheduler at the start every minute
 while [ true ]; do
-    current_second=$(date +%S)
-    sleep_time=$((60 - current_second))
+    # Get current time as a floating-point number (seconds + nanoseconds)
+    current_time=$(date +%S.%N)
+
+    # Compute how many seconds to sleep until the next full minute
+    sleep_time=$(echo "60 - $current_time" | bc)
 
 #    echo "Waiting $sleep_time seconds for the next full minute..."
-    sleep $sleep_time  # Wait until `XX:YY:00`
+    sleep $sleep_time  # Wait until `XX:YY:00.000`
 
-#    echo "Running scheduler at: $(date +'%Y-%m-%d %H:%M:%S')"
-    php artisan schedule:run --verbose --no-interaction
+#    echo "Running scheduler at: $(date +'%Y-%m-%d %H:%M:%S.%3N')"
+    php artisan schedule:run --verbose --no-interaction &
 done
